50077fd238f065b805576516c635f3252710fc63
Jean Vuillet <vuillet433@hotmail.com>
2024-04-19 20:08:10 -0500

    accountcomp1

--- front/src/Pages/User/User.jsx
+++ front/src/Pages/User/User.jsx
@@ -2,8 +2,20 @@ import './User.scss'
 
 export function User(){
  return(
-    <div className="user">
-        user
+    <>
+    <div className="header">
+        <h1>Welcome back
+            <br/>
+            Tonny
+        </h1>
+        <button class="edit-button">Edit Name</button>
     </div>
+    <div className="accoutsSection">
+        <h2>Accounts</h2>
+        <div className="accounts">
+            
+        </div>
+    </div>
+    </>
  )
 }
